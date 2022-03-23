8d1e27015d69a6d46f95074e6b5a1ebc5e2e2c09
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-23 16:17:44 +0100

    Creating components and liking them

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Character from "./components/Character.svelte";
+	import Search from "./components/Search.svelte";
+</script>
+<h1>Breaking Bad</h1>
+<h2>Characters</h2>
+<Search />
+<Character />
