41739723fa1a8ddf402997c533d01aa16ac698eb
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-23 16:32:02 +0100

    Getting the data

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,13 @@
 <script>
+	import { onMount } from 'svelte';
 	import Character from "./components/Character.svelte";
 	import Search from "./components/Search.svelte";
+	let data  = [];
+	onMount(async () => {
+		const response = await fetch("https://breakingbadapi.com/api/characters")
+		data = await response.json();
+	})
+
 </script>
 <h1>Breaking Bad</h1>
 <h2>Characters</h2>
